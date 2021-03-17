ef30e88df226e43cfd6c3c9e37ae66f4bc118744
Andy240881 <19980303andy@gmail.com>
2021-03-17 19:38:12 +0800

    debug

---
+++ Debug/FreeRTOS/queue.d
@@ -0,0 +1,34 @@
+FreeRTOS/queue.o: ../FreeRTOS/queue.c \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/FreeRTOS.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/FreeRTOSConfig.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/projdefs.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/portable.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/deprecated_definitions.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/mpu_wrappers.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/task.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/list.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/queue.h \
+ /home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/task.h
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/FreeRTOS.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/FreeRTOSConfig.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/projdefs.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/portable.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/deprecated_definitions.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/mpu_wrappers.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/task.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/list.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/queue.h:
+
+/home/andy/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_p76091307/FreeRTOS/include/task.h:
